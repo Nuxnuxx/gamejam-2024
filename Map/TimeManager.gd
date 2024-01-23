59364607fb7f46bdc30e9760b2332e03ba40467b
Enzo-py <85191552+Enzo-py@users.noreply.github.com>
2024-01-23 22:22:36 +0100

    holy score

--- Map/TimeManager.gd
+++ Map/TimeManager.gd
@@ -1,14 +1,25 @@
 extends Node
 
+@onready var holy_bar = $"../CanvasLayer/HolyBar"
 @onready var spawner = $"../Spawner"
 const WaweManager = preload("res://game_manager/wawe_manager.gd")
 
 var current_wawe = 0
 var generate_wave = false
 
+var holy_score = 0
+var holy_timer = 4
+var holy_income = 4
+var holy_cd = false
+
 func _process(delta):
 	if spawner.wawe_is_running:
-		print('wave running')
+		if !holy_cd:
+			holy_cd = true
+			holy_score += holy_income * int(holy_score < 100)
+			holy_bar.value = holy_score
+			await get_tree().create_timer(holy_timer).timeout
+			holy_cd = false
 		
 	elif !generate_wave && !spawner.wawe_is_running:
 		generate_wave = true
@@ -17,7 +28,6 @@ func _process(delta):
 		current_wawe += 1
 		generate_wave = false
 		var wawe = WaweManager.generate_wawe(current_wawe)
-		print("summon wave", current_wawe)
 		spawner.instance_entities_from_wawe(wawe)
 	else:
 		generate_wave = false
