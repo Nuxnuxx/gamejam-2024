e0cd56654069d9ce454f6181ae072387f3af7c39
Enzo-py <85191552+Enzo-py@users.noreply.github.com>
2024-01-24 07:12:13 +0100

    Music

--- Map/TimeManager.gd
+++ Map/TimeManager.gd
@@ -4,6 +4,8 @@ extends Node
 @onready var spawner = $"../Spawner"
 @onready var wave_counter = $"../CanvasLayer/WaveCounter"
 @onready var directional_light_2d = $"../DirectionalLight2D"
+@onready var battle = $sound/battle
+@onready var day = $sound/day
 
 const WaweManager = preload("res://game_manager/wawe_manager.gd")
 
@@ -16,10 +18,19 @@ var holy_timer = 4
 var holy_income = 4
 var holy_cd = false
 var val = 0
+var init_day = true
+var init_night = true
 
 func _process(delta):
 	if spawner.wawe_is_running:
 		directional_light_2d.energy = log(0.5 + 3*val)
+		
+		if init_night:
+			day.stop()
+			battle.play()
+			init_day = true
+			init_night = false
+			
 		if val < 2: val += delta
 		if !holy_cd:
 			holy_cd = true
@@ -29,11 +40,16 @@ func _process(delta):
 			holy_cd = false
 		
 	elif !generate_wave && !spawner.wawe_is_running:
+		if init_day:
+			battle.stop()
+			day.play()
+			init_night = true
+			init_day = false
 		generate_wave = true
-		directional_light_2d.energy = log(val + 0.5)
+		directional_light_2d.energy = log(2*val + 0.5)
 		if val > 0: val -= delta
 		
-		await get_tree().create_timer(5).timeout
+		await get_tree().create_timer(10).timeout
 		if spawner.wawe_is_running: return
 		current_wawe += 1
 		wave_counter.text = str(current_wawe)
